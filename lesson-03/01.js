4aa19b71859eb02cddd5935fa733cc64a10052cd
Const1011100 <const1011100@gmail.com>
2023-12-23 23:38:52 +0200

    hw-js_3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,12 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const flag = colors.filter(elem => {
+        if (elem === "черный" || elem === "красный" || elem === "желтый") {
+            return elem;
+        }
+    });
+    return flag.join('-');
+}
+
+console.log(createColorString());
